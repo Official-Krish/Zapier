fef8f223c63789ab07b7ade51dbf8caad760e29b
Official-Krish <krishanand0103@gmail.com>
2024-08-07 01:46:18 +0530

    added acknowledgement

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { Kafka } from "kafkajs";
+import { parse } from 'path';
 const prisma = new PrismaClient()
 
 const TOPIC_NAME = "zapier-events"
@@ -16,13 +17,21 @@ async function main() {
         topic: "zapier-events", fromBeginning: true
     })
     await consumer.run({
+        autoCommit: false,
         eachMessage: async ({ topic, partition, message }) => {
           console.log({
             partition,
             offset: message.offset,
             value: message?.value?.toString(),
           })
+
           await new Promise(resolve => setTimeout(resolve, 2000))
+
+          await consumer.commitOffsets([{
+            topic : TOPIC_NAME,
+            partition : partition,
+            offset : (parseInt(message.offset) + 1).toString()
+          }])
         },
     })
 }
