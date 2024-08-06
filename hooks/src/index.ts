78b93b3c0d8be8f6e1d314cd829370a8feda3959
Official-Krish <krishanand0103@gmail.com>
2024-08-06 19:22:02 +0530

    create zap

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,6 +1,30 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 const app = express();
+const client = new PrismaClient();
+
+app.use(express.json());
 
 app.post('/hooks/catch/:userId/:zapId', async (req, res) => {
     const { userId, zapId } = req.params;
+    const body = req.body;
+
+    await client.$transaction(async (tx) => {
+        const run = await client.zapRun.create({
+            data: {
+                zapId : zapId,
+                metadata : body,
+            }
+        });
+
+        await client.zapRunOutbox.create({
+            data: {
+                zapRunId : run.id,
+            }
+        });
+        res.json({
+            msg : 'success'
+        })
+    });
 });
+app.listen(3000);
