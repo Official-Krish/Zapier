a11609ef13edb577ded64bfa6a8c166c7141af76
Official-Krish <krishanand0103@gmail.com>
2024-08-10 00:05:31 +0530

    landing paege

---
+++ frontend/app/components/hero.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { PrimaryButton } from "./buttons/primaryBtn"
+import { SecondaryButton } from "./buttons/secondaryBtn"
+import { Features } from "./features"
+
+export const Hero = () => {
+    return <div>
+        <div className="flex justify-center items-center">
+            <div className="text-6xl font-bold text-center mt-20 max-w-4xl">
+                Automate as fast as you can type
+            </div>
+        </div>
+        <div className="flex justify-center items-center">
+            <div className="text-xl font-medium text-center mt-7 max-w-3xl">
+                AI gives you automation superpowers, and Zapier puts them to work. Pairing AI and Zapier helps you turn ideas into workflows and bots that work for you.
+            </div>
+        </div>
+
+        <div className="flex pt-8 justify-center items-center">
+            <PrimaryButton size="big" onClick={() => {}}>
+                Start free with email
+            </PrimaryButton>
+            
+            <div className="pl-4">
+                <SecondaryButton size="big" onClick={() => {}}>
+                    Start free with Google 
+                </SecondaryButton>
+            </div>
+            
+        </div>
+
+        <div className="flex justify-center">
+            <Features title="Free forever" subtitle="for core features"></Features>
+            <Features title="More apps" subtitle="than any other platform"></Features>
+            <Features title="Cutting edge" subtitle="AI features"></Features>
+        </div>
+    </div>
+}
