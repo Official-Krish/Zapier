c2168401728a114c19e72613c4a6085bb92a4fa0
Official-Krish <krishanand0103@gmail.com>
2024-08-09 23:01:11 +0530

    init

---
+++ frontend/app/components/Appbar.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { useRouter } from "next/navigation"
+import { LinkButton } from "./buttons/LinkBtn"
+import { PrimaryButton } from "./buttons/primaryBtn"
+
+export const Appbar = () => {
+    const router = useRouter()
+    return <div className="flex border-b justify-between py-2 px-4 items-center">
+        <div className="flex flex-col justify-center">
+            Zapier
+        </div>
+
+        <div className="flex px-6">
+            <div className="pr-4">
+                <LinkButton onClick={() => alert("Contact Sales")}>
+                    Contact Sales
+                </LinkButton>
+            </div>
+            <div className="pr-4">
+                <LinkButton onClick={() => router.push("/login")}>
+                    Login
+                </LinkButton>
+            </div>
+            <PrimaryButton onClick={() => router.push("/signup")}>
+                Sign up
+            </PrimaryButton>
+
+        </div>
+    </div>
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import { Appbar } from "./components/Appbar";
+
+export default function Home() {
+  return (
+    <main className="">
+      <Appbar/>
+      {/* <Hero/>
+      <heroVideo/> */}
+    </main>
+  );
+}
