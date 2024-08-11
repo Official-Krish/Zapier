92ced82be0b299ebd1549a5ac5159540e31bea84
Official-Krish <krishanand0103@gmail.com>
2024-08-11 23:53:47 +0530

    create page

--- frontend/app/zap/create/page.tsx
+++ frontend/app/zap/create/page.tsx
@@ -6,16 +6,24 @@ import { ZapCell } from "@/app/components/zapCell";
 import { BACKEND_URL } from "@/app/config";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function useAvailableActionsAndTriggers() {
     const [availableActions, setAvailableActions] = useState([]);
     const [availableTriggers, setAvailableTriggers] = useState([]);
 
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/api/v1/trigger/available`)
+            .then(x => setAvailableTriggers(x.data))
+
+        axios.get(`${BACKEND_URL}/api/v1/action/available`)
+            .then(x => setAvailableActions(x.data))
+    }, [])
     return {
         availableActions,
         availableTriggers
     }
+
 }
 
 export default function() {
@@ -30,7 +38,6 @@ export default function() {
         index: number;
         availableActionId: string;
         availableActionName: string;
-        metadata: any;
     }[]>([]);
     const [selectedModalIndex, setSelectedModalIndex] = useState<null | number>(null);
 
@@ -47,7 +54,6 @@ export default function() {
                     "triggerMetadata": {},
                     "actions": selectedActions.map(a => ({
                         availableActionId: a.availableActionId,
-                        actionMetadata: a.metadata
                     }))
                 }, {
                     headers: {
@@ -77,7 +83,6 @@ export default function() {
                             index: a.length + 2,
                             availableActionId: "",
                             availableActionName: "",
-                            metadata: {}
                         }])
                     }}><div className="text-2xl">
                         +
@@ -86,5 +91,67 @@ export default function() {
             </div>
         </div>
         
-    </div>
+        {selectedModalIndex && <Modal availableItems={selectedModalIndex === 1 ? availableTriggers : availableActions} onSelect={(props: null | { name: string; id: string; }) => {
+            if (props === null) {
+                setSelectedModalIndex(null);
+                return;
+            }
+            if (selectedModalIndex === 1) {
+                setSelectedTrigger({
+                    id: props.id,
+                    name: props.name
+                })
+            } else {
+                setSelectedActions(a => {
+                    let newActions = [...a];
+                    newActions[selectedModalIndex - 2] = {
+                        index: selectedModalIndex,
+                        availableActionId: props.id,
+                        availableActionName: props.name,
+                    }
+                    return newActions
+                })
+            }
+            setSelectedModalIndex(null);
+        }}  index={selectedModalIndex} />}
+    </div>            
+
 }
+
+function Modal({ index, onSelect, availableItems }: { index: number, onSelect: (props: null | { name: string; id: string; }) => void, availableItems: {id: string, name: string, image: string}[] }) {
+
+    return <div className="fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-slate-100 bg-opacity-70 flex">
+        <div className="relative p-4 w-full max-w-2xl max-h-full">
+            <div className="relative bg-white rounded-lg shadow ">
+                <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
+                    <div className="text-xl">
+                        Select {index === 1 ? "Trigger" : "Action"}
+                    </div>
+                    <button onClick={() => {
+                        onSelect(null);
+                    }} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal">
+                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                        <span className="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <div className="p-4 md:p-5 space-y-4"> 
+
+                     <div>{availableItems.map(({id, name, image}) => {
+                            return <div onClick={() => {
+                                onSelect({
+                                    name,
+                                    id,
+                                })
+                            }} className="flex border p-4 cursor-pointer hover:bg-slate-100">
+                                <img src={image} width={30} className="rounded-full" /> <div className="flex flex-col justify-center"> {name} </div>
+                            </div>
+                        })}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+}
+
