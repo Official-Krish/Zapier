e81c908e41dad8894ae120ec3950b4c877a5f6be
Official-Krish <krishanand0103@gmail.com>
2024-08-11 23:58:56 +0530

    fix

--- frontend/app/zap/create/page.tsx
+++ frontend/app/zap/create/page.tsx
@@ -118,7 +118,7 @@ export default function() {
 
 }
 
-function Modal({ index, onSelect, availableItems }: { index: number, onSelect: (props: null | { name: string; id: string; }) => void, availableItems: {id: string, name: string, image: string}[] }) {
+function Modal({ index, onSelect, availableItems }: { index: number, onSelect: (props: null | { name: string; id: string; }) => void, availableItems: {id: string, name: string, Image: string}[] }) {
 
     return <div className="fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-slate-100 bg-opacity-70 flex">
         <div className="relative p-4 w-full max-w-2xl max-h-full">
@@ -138,14 +138,14 @@ function Modal({ index, onSelect, availableItems }: { index: number, onSelect: (
                 </div>
                 <div className="p-4 md:p-5 space-y-4"> 
 
-                     <div>{availableItems.map(({id, name, image}) => {
+                     <div>{availableItems.map(({id, name, Image}) => {
                             return <div onClick={() => {
                                 onSelect({
                                     name,
                                     id,
                                 })
                             }} className="flex border p-4 cursor-pointer hover:bg-slate-100">
-                                <img src={image} width={30} className="rounded-full" /> <div className="flex flex-col justify-center"> {name} </div>
+                                <img src={Image} width={30} className="rounded-full" /> <div className="flex flex-col justify-center"> {name} </div>
                             </div>
                         })}</div>
                 </div>
