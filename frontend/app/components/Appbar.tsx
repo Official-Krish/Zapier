0cf8181439953fc93e1cc2771fa1ab8c4116c472
Official-Krish <krishanand0103@gmail.com>
2024-08-14 13:10:17 +0530

    added avatar and dropdown

--- frontend/app/components/Appbar.tsx
+++ frontend/app/components/Appbar.tsx
@@ -2,23 +2,34 @@
 import { useRouter } from "next/navigation"
 import { LinkButton } from "./buttons/LinkBtn"
 import { PrimaryButton } from "./buttons/primaryBtn"
+import { useState } from "react"
+import Avatar from "./avatar"
+import  Dropdown  from "./dropdown"
+
 
 export const Appbar = () => {
     const router = useRouter()
+    const [dropdownOpen, setDropdownOpen] = useState<boolean>(false)
+
+    const toggleDropdown = () => setDropdownOpen(prev => !prev)
+
     return (
         <div className="flex border-b justify-between py-2 px-4 items-center">
             <div className="flex flex-col justify-center text-2xl font-bold">
                 Zapier
             </div>
-                {localStorage.getItem("token") ? (
-                    avatar()
-                ) : (
-                    <>
+            {localStorage.getItem("token") ? (
+                <div className="relative">
+                    <Avatar onClick={toggleDropdown} />
+                    {dropdownOpen && <Dropdown />}
+                </div>
+            ) : (
+                <>
                     <div className="pr-4 flex justify-end">
                         <div className="flex justify-between pr-4">
-                        <LinkButton onClick={() => alert("Contact Sales")}>
-                            Contact Sales
-                        </LinkButton>
+                            <LinkButton onClick={() => alert("Contact Sales")}>
+                                Contact Sales
+                            </LinkButton>
                         </div>
                         <div className="pr-4">
                             <LinkButton onClick={() => router.push("/login")}>
@@ -28,19 +39,9 @@ export const Appbar = () => {
                         <PrimaryButton onClick={() => router.push("/signup")}>
                             Sign up
                         </PrimaryButton>
-                        </div>
-                    </>
-                )}
-            </div>
-    )
-}
-
-function avatar() {
-    return (
-        <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-            <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path>
-            </svg>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
---
+++ frontend/app/components/dropdown.tsx
@@ -0,0 +1,23 @@
+
+const Dropdown = () => {
+    return (
+        <div id="dropdownHover" className="z-10 bg-black divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute">
+            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+                <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                </li>
+                <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                </li>
+                <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Dropdown
