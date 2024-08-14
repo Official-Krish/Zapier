499262a3f5b19f51176f8cbd171389cd42da3761
Official-Krish <krishanand0103@gmail.com>
2024-08-14 23:55:51 +0530

    added sidebar

--- frontend/app/components/Appbar.tsx
+++ frontend/app/components/Appbar.tsx
@@ -5,18 +5,37 @@ import { PrimaryButton } from "./buttons/primaryBtn"
 import { useState } from "react"
 import Avatar from "./avatar"
 import  Dropdown  from "./dropdown"
-
+import { Sidebar } from "./sidebar"
+import { Menu } from "./icons/menu"
 
 export const Appbar = () => {
     const router = useRouter()
     const [dropdownOpen, setDropdownOpen] = useState<boolean>(false)
+    const [sidebarOpen, setsidebarOpen] = useState<boolean>(false)
 
     const toggleDropdown = () => setDropdownOpen(prev => !prev)
+    const toggleSideBar = () => setsidebarOpen(prev => !prev)
 
     return (
         <div className="flex border-b justify-between py-2 px-4 items-center">
             <div className="flex flex-col justify-center text-2xl font-bold">
-                Zapier
+            <div className="flex">
+            {localStorage.getItem("token") !== null ? (
+                <>
+                    <div onClick={toggleSideBar} className="cursor-pointer px-4 py-1">
+                        <Menu />
+                    </div>
+                    {sidebarOpen && <Sidebar />}
+                    <div>
+                        Zapier
+                    </div>
+                </>
+            ) : (
+                <div>
+                    Zapier
+                </div>
+            )}
+            </div>
             </div>
             {localStorage.getItem("token") !== null ? (
                 <div className="pr-6">
--- frontend/app/components/buttons/primaryBtn.tsx
+++ frontend/app/components/buttons/primaryBtn.tsx
@@ -1,7 +1,15 @@
 import { ReactNode } from "react"
 
-export const PrimaryButton = ({ children, onClick, size = "small" }: { children: ReactNode, onClick: () => void, size?: "big" | "small"}) => {
-    return <div onClick={onClick} className={`${size === "small" ? "text-sm" : "text-xl"} ${size === "small" ? "px-8 py-2" : "px-10 py-3"} cursor-pointer  hover:shadow-md bg-orange-600 text-white rounded-full text-center flex justify-center flex-col`}>
-        {children}
+export const PrimaryButton = ({ children, onClick, size = "small" , logo}: { children: ReactNode, onClick: () => void, size?: "big" | "small" | "large" , logo ?: ReactNode}) => {
+    return <div onClick={onClick} className={`${size === "small" ? "text-sm" : "text-xl"} ${size === "small" ? "px-8 py-2" : "px-10 py-3"} 
+    ${size == "large" ? "px-16 py-1 rounded-lg" : ""}
+    cursor-pointer  hover:shadow-md bg-orange-600 text-white rounded-full text-center flex justify-center flex-col`} 
+    >
+        <div className="flex justify-between items-center">
+            {logo}
+            <div className="pl-3">
+                {children}
+            </div>
+        </div>
     </div>
 }
